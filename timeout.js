84887952935f9fd2a42ba661e072fcb5ce665fd7
matt6390 <matthewstone639@gmail.com>
2018-02-13 21:29:23 -0600

    done for day

---
+++ timeout.js
@@ -0,0 +1,10 @@
+setTimeout(function() {
+  console.log("First Task Done!!");
+  setTimeout(function() {
+    console.log("Second Task Done!!");
+    setTimeout(function() {
+      console.log("Third Task Done!!");
+    }, 2000);
+  }, 2000);
+}, 2000);
+
